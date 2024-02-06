d4c30682b1845e936615893bfed631d557e9fd01
github-action-benchmark <github@users.noreply.github.com>
2024-02-06 09:07:31 +0000

    add msal-node client-credential Regression Test (benchmarkjs) benchmark result for e7a55511680aec602310f63db0bb5b7d2b07fab3

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1700768247315,
+  "lastUpdate": 1707210450938,
   "repoUrl": "https://github.com/dzhavat/microsoft-authentication-library-for-js",
   "entries": {
     "msal-node client-credential Regression Test": [
@@ -38,6 +38,44 @@ window.BENCHMARK_DATA = {
             "range": "±1.83%",
             "unit": "ops/sec",
             "extra": "224 samples"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "thomas.norling@microsoft.com",
+            "name": "Thomas Norling",
+            "username": "tnorling"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "e7a55511680aec602310f63db0bb5b7d2b07fab3",
+          "message": "Fix electron webpack samples (#6864)\n\nThe electron webpack samples' transforms weren't updated when we changed\r\nto .cjs extensions which breaks native module imports (node extensions).\r\nThis PR addresses by adding .cjs to the list of files to transform",
+          "timestamp": "2024-02-01T14:40:23-08:00",
+          "tree_id": "f52a9b6647fc2a3d908aae83ec2062eff9175110",
+          "url": "https://github.com/dzhavat/microsoft-authentication-library-for-js/commit/e7a55511680aec602310f63db0bb5b7d2b07fab3"
+        },
+        "date": 1707210449315,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "ConfidentialClientApplication#acquireTokenByClientCredential-fromCache-resourceIsFirstItemInTheCache",
+            "value": 182196,
+            "range": "±1.67%",
+            "unit": "ops/sec",
+            "extra": "220 samples"
+          },
+          {
+            "name": "ConfidentialClientApplication#acquireTokenByClientCredential-fromCache-resourceIsLastItemInTheCache",
+            "value": 183836,
+            "range": "±1.77%",
+            "unit": "ops/sec",
+            "extra": "220 samples"
           }
         ]
       }
